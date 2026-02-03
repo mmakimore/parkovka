cc2e16a48c10a80542856ca2b9fdcd43d6f0072c
sdcsdcsdc <kurguzovgleb.10@gmail.com>
2026-02-03 21:45:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-# Конфигурация бота
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8569990381:AAG9wr0L9g5pUn9bp1H2wwfDZju3vIuVOBI")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "7884533080"))
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "tyufj123")
