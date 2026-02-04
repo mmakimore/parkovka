71f1075f84e77e874e6fe5d85b38c28d7caa1ae7
sdcsdcsdc <kurguzovgleb.10@gmail.com>
2026-02-04 10:36:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8569990381:AAG9wr0L9g5pUn9bp1H2wwfDZju3vIuVOBI")
-ADMIN_CHAT_IDS = list(map(int, os.getenv("ADMIN_CHAT_IDS", "7884533080").split(',')))
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "7884533080"))
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "tyufj123")
